f9cf4e4de98299fe6d5f09fa060ab8d7c9efef37
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-17 13:17:44 -0400

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,7 +1,7 @@
 def two_sum_pairs(numbers, target):
     answer = []
-    for i in range (0,len(numbers)-1):
-        for j in range(i+1,len(numbers)-1):
+    for i in range (0,len(numbers)):
+        for j in range(i+1,len(numbers)):
             if(numbers[i]+numbers[j]==target):
                 answer.append({numbers[i],numbers[j]})
     return answer
